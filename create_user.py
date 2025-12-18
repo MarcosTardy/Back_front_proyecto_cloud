a082536aa36cecae3a83c7e1b203a26c5539f82b
MarcosTardy <marcostardsan@gmail.com>
2025-12-18 11:08:46 +0100

    Cambio en la bd

--- create_user.py
+++ create_user.py
@@ -7,7 +7,7 @@ def create_user():
 
     user = User(
         username="admin",
-        hashed_password=hash_password("12345678")
+        password_hash=hash_password("12345678")
     )
 
     db.add(user)
--- models.py
+++ models.py
@@ -6,4 +6,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    password_hash = Column(String)
